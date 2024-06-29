3389316c4930386d675941613ecd5bd4c4059b77
Stream <1542763342@qq.com>
2024-06-30 01:25:07 +0800

    Add files via upload

---
+++ FieldFunc.java
@@ -0,0 +1,25 @@
+package functional;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public interface FieldFunc<Self>
+{
+    public default Self also(Consumer<Self> block)
+    {
+        block.accept((Self) this);
+        return (Self) this;
+    }
+
+    public default <R> R let(Function<Self, R> block)
+    {
+        return block.apply((Self) this);
+    }
+
+    public default Self apply(Consumer<Self> block)
+    {
+        block.accept((Self) this);
+        return (Self) this;
+    }
+
+}
